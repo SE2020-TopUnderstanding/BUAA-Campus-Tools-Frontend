1d25c92dbb01a82168af6d6ebb4cafd80e603877
Michael-Steven <46148048+Michael-Steven@users.noreply.github.com>
2020-04-15 12:58:28 +0800

    add json file to ScorePage (#29)

--- jiaowu_assistent/lib/pages/ScorePage.dart
+++ jiaowu_assistent/lib/pages/ScorePage.dart
@@ -1,19 +1,33 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Grade {
-  final String name;
-  final double credit;
-  final int score;
-  bool selected = false; //默认为未选中
-  Grade(
-    this.name,
-    this.credit,
-    this.score,
-  );
+  String name;
+  double credit;
+  int score;
+
+  Grade(this.name, this.credit, this.score);
+
+  Grade.fromJson(Map<String, dynamic> json) {
+    name = json['course_name'];
+    credit = json['credit'];
+    score = json['score'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['course_name'] = this.name;
+    data['credit'] = this.credit;
+    data['score'] = this.score;
+    return data;
+  }
 }
 
-List<Grade> grades = <Grade>[Grade('计算机网络', 3, 90), Grade('软件工程', 2, 93)];
+//List<Grade> grades = <Grade>[Grade('计算机网络', 3, 90), Grade('软件工程', 2, 93)];
 
 class ScorePage extends StatelessWidget {
   @override
@@ -22,18 +36,6 @@ class ScorePage extends StatelessWidget {
     double sumScore = 0;
     double sumCredit = 0;
 
-    for (int i = 0; i < grades.length; i++) {
-      dateRows.add(DataRow(
-        selected: grades[i].selected,
-        cells: [
-          DataCell(Text('${grades[i].name}')),
-          DataCell(Text('${grades[i].credit}')),
-          DataCell(Text('${grades[i].score}')),
-        ],
-      ));
-      sumScore += grades[i].score;
-      sumCredit += grades[i].credit;
-    }
     return new Scaffold(
         appBar: new AppBar(
           leading: IconButton(
@@ -44,44 +46,100 @@ class ScorePage extends StatelessWidget {
           backgroundColor: Colors.lightBlue,
           automaticallyImplyLeading: false,
         ),
-        body: Container(
-          padding: EdgeInsets.all(16),
-          child: ListView(
-            children: <Widget>[
-              DataTable(
-                  sortColumnIndex: 1,
-                  sortAscending: true,
-                  columns: [
-                    DataColumn(label: Text('科目')),
-                    DataColumn(label: Text('学分')),
-                    DataColumn(label: Text('成绩')),
-                  ],
-                  rows: dateRows
-              )
-            ],
-          ),
-        ),
-        floatingActionButton: RaisedButton(
-          color: Colors.lightBlue,
-          onPressed: () {},
-          onHighlightChanged: (b) {
-            //If your press down the button ,b is true
-            //If your release up the button,b is false
-          },
-//          textTheme: ButtonTextTheme.primary,
-          elevation: 4.0,
-          highlightElevation: 8.0,
-          disabledElevation: 0.0,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(
-              color: Colors.black,
-              width: 3.0,
-              style: BorderStyle.none,
+        body: Score());
+  }
+}
+
+class Score extends StatelessWidget {
+
+  List<Grade> grades = <Grade>[Grade('计算机网络', 3, 90), Grade('软件工程', 2, 93)];
+
+  Widget build(BuildContext context) {
+    Map<String, dynamic> data;
+    List<DataRow> dateRows = [];
+    double sumScore = 0;
+    double sumCredit = 0;
+
+    for (int i = 0; i < 2; i++) {
+      dateRows.add(DataRow(
+//        selected: grades[i].selected,
+        cells: [
+          DataCell(Text('${grades[i].name}')),
+          DataCell(Text('${grades[i].credit}')),
+          DataCell(Text('${grades[i].score}')),
+        ],
+      ));
+//      sumScore += grades[i].score;
+//      sumCredit += grades[i].credit;
+    }
+    return new Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child:
+        ListView(
+          children: <Widget>[
+            DataTable(
+                sortColumnIndex: 1,
+                sortAscending: true,
+                columns: [
+                  DataColumn(label: Text('科目')),
+                  DataColumn(label: Text('学分')),
+                  DataColumn(label: Text('成绩')),
+                ],
+                rows: dateRows),
+            RaisedButton(
+              color: Colors.lightBlue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
+              child: Text(
+                '刷新',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    letterSpacing: 20,
+                    fontSize: 20,
+                    color: Colors.black54),
+              ),
+              onPressed: () {},
             ),
-            borderRadius: BorderRadius.circular(12.0),
-          ),
-          animationDuration: const Duration(milliseconds: 200),
-          child: Text('刷新'),
-        ));
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AverageScore extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new SizedBox(
+      width: 200.0,
+      height: 50.0,
+    );
+  }
+}
+
+class RefreshButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new SizedBox(
+      width: 200.0,
+      height: 50.0,
+      child: new RaisedButton(
+        color: Colors.lightBlue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+        child: Text(
+          '刷新',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              letterSpacing: 20,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: Colors.black54),
+        ),
+        onPressed: () {},
+      ),
+    );
   }
 }
