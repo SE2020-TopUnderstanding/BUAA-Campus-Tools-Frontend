2b54d090aca2c2e3ca867ceab77b0919a37c290b
Michael-Steven <727950731@qq.com>
2020-04-25 22:38:43 +0800

    finish update dialog

--- jiaowu_assistent/lib/main.dart
+++ jiaowu_assistent/lib/main.dart
@@ -7,11 +7,20 @@ import 'package:jiaowuassistent/pages/FirstPage.dart';
 import 'package:async/async.dart';
 import 'package:jiaowuassistent/pages/HomePage.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GlobalUser.init();
   runApp(MyApp());
+  if (Platform.isAndroid) {
+    //设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，
+    //覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
 }
 
 class MyApp extends StatelessWidget {
