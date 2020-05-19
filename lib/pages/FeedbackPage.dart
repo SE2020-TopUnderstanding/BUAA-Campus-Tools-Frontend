618f453735305c7d8184fc9dd98344392210761f
Michael-Steven <727950731@qq.com>
2020-05-19 12:15:01 +0800

    集成极光推送并跑通demo

--- lib/pages/FeedbackPage.dart
+++ lib/pages/FeedbackPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:jiaowuassistent/pages/FeedbackDetailPage.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+import 'package:jpush_flutter/jpush_flutter.dart';
 
 class FeedbackPage extends StatefulWidget {
   @override
@@ -10,6 +13,41 @@ class FeedbackPage extends StatefulWidget {
 
 class _FeedbackPage extends State<FeedbackPage> {
   List<String> items = ["课表查询", "DDL查询", "空教室查询"];
+  String debugLable = 'Unknown';
+
+  /*错误信息*/
+  final JPush jpush = new JPush();
+
+  /* 初始化极光插件*/
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState(); /*极光插件平台初始化*/
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+
+    try {
+      /*监听响应方法的编写*/
+      jpush.addEventHandler(
+          onReceiveNotification: (Map<String, dynamic> message) async {
+        print(">>>>>>>>>>>>>>>>>flutter 接收到推送: $message");
+        setState(() {
+          debugLable = "接收到推送: $message";
+        });
+      });
+    } on PlatformException {
+      platformVersion = '平台版本获取失败，请检查！';
+    }
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      debugLable = platformVersion;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +90,29 @@ class _FeedbackPage extends State<FeedbackPage> {
                   );
                 },
               ),
-            )
+            ),
+            RaisedButton(
+                child: new Text(
+                  '点击发送推送消息\n',
+                ),
+                onPressed: () {
+                  /*三秒后出发本地推送*/
+                  var fireDate = DateTime.fromMillisecondsSinceEpoch(
+                      DateTime.now().millisecondsSinceEpoch + 3000);
+                  var localNotification = LocalNotification(
+                    id: 234,
+                    title: '我是推送测试标题',
+                    buildId: 1,
+                    content: '看到了说明已经成功了',
+                    fireTime: fireDate,
+                    subtitle: '一个测试',
+                  );
+                  jpush.sendLocalNotification(localNotification).then((res) {
+                    setState(() {
+                      debugLable = res;
+                    });
+                  });
+                }),
           ],
         ));
   }
