0b42044634bbf9a7feb110f2ed73dd7f5714c101
Michael-Steven <727950731@qq.com>
2020-06-02 18:23:53 +0800

    美化功能选择页面，添加课程评价时间

--- lib/pages/FunctionsPage.dart
+++ lib/pages/FunctionsPage.dart
@@ -10,29 +10,38 @@ import 'package:jiaowuassistent/pages/CalendarPage.dart';
 class FunctionsPage extends StatelessWidget {
   // 图标函数，其中page 参数需要给出页面函数
 
-  Column _block(BuildContext context, Icon icon, Text text, Widget page) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 60,
-          child: RaisedButton(
-            shape: CircleBorder(),
-            child: icon,
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => page));
-            },
-          ),
+  Container _block(BuildContext context, Icon icon, Text text, Widget page) {
+    return Container(
+      height: 60,
+      width: 600,
+      decoration: BoxDecoration(
+        border: new Border.all(width: 2.0, color: Colors.black12),
+        borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
+      ),
+      child: RaisedButton(
+        color: Colors.grey[100],
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              width: 20,
+            ),
+            icon,
+            SizedBox(
+              width: 30,
+            ),
+            text
+          ],
         ),
-        SizedBox(
-          height: 10,
-        ),
-        text,
-      ],
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => page));
+        },
+      ),
     );
   }
-
-  // 目前的设计是一行一行，然后每行三列，一列里面button + text
 
   @override
   Widget build(BuildContext context) {
@@ -44,71 +53,82 @@ class FunctionsPage extends StatelessWidget {
       ),
       backgroundColor: Colors.grey[100],
       body: new Container(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: EdgeInsets.only(left: 50, top: 40, right: 50, bottom: 40),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            SizedBox(
-              height: 100,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _block(
-                    context,
-                    Icon(
-                      Icons.school,
-                      size: 40,
-                    ),
-                    Text("成绩查询"),
-                    ScorePage()),
-                _block(
-                    context,
-                    Icon(
-                      Icons.business,
-                      size: 40,
-                    ),
-                    Text("空教室查询"),
-                    EmptyRoomPage()),
-                _block(
-                    context,
-                    Icon(
-                      Icons.check_circle_outline,
-                      size: 40,
-                    ),
-                    Text("课程中心查询"),
-                    CourseCenterPage()),
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-//                _block(context, Icon(Icons.school, size: 40,), Text("课程评价"), ScorePage()),
-
-                _block(
-                    context,
-                    Icon(
-                      Icons.date_range,
-                      size: 40,
-                    ),
-                    Text("课表查询"),
-                    CourseTablePage()),
-                _block(
-                    context,
-                    Icon(
-                      Icons.border_color,
-                      size: 40,
-                    ),
-                    Text("课程评价"),
-                    CourseEvaluationPage()),
-
-                _block(context, Icon(Icons.calendar_today, size: 40,), Text("校历"), CalendarPage()),
-
-              ],
-            )
+            _block(
+                context,
+                Icon(
+                  Icons.school,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "成绩查询",
+                  style: TextStyle(fontSize: 20),
+                ),
+                ScorePage()),
+            _block(
+                context,
+                Icon(
+                  Icons.business,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "空教室查询",
+                  style: TextStyle(fontSize: 20),
+                ),
+                EmptyRoomPage()),
+            _block(
+                context,
+                Icon(
+                  Icons.check_circle_outline,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "课程中心查询",
+                  style: TextStyle(fontSize: 20),
+                ),
+                CourseCenterPage()),
+            _block(
+                context,
+                Icon(
+                  Icons.date_range,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "课表查询",
+                  style: TextStyle(fontSize: 20),
+                ),
+                CourseTablePage()),
+            _block(
+                context,
+                Icon(
+                  Icons.assignment,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "课程评价",
+                  style: TextStyle(fontSize: 20),
+                ),
+                CourseEvaluationPage()),
+            _block(
+                context,
+                Icon(
+                  Icons.calendar_today,
+                  color: Color(0xFF1565C0),
+                  size: 40,
+                ),
+                Text(
+                  "校历",
+                  style: TextStyle(fontSize: 20),
+                ),
+                CalendarPage()),
           ],
         ),
       ),
